1e91113b163dc4787a5815c4960a42739710ef9f
Christiane <christianegirao@gmail.com>
2017-11-18 17:06:03 -0300

    Projeto criado

---
+++ src/main/java/com/cgac/pastoral/PastoralApplication.java
@@ -0,0 +1,12 @@
+package com.cgac.pastoral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastoralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastoralApplication.class, args);
+	}
+}
